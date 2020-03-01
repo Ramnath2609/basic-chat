3563ef9e7182738523ee8f3dc6dcf1889881ce8b
Ramnath K <ramnath2609@gmail.com>
2020-03-01 19:16:34 +0530

    latest commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const setUpDB = () => {
-    mongoose.connect('mongodb://localhost:27017/basic-chat', { useNewUrlParser : true, useUnifiedTopology : true }, () => {
+    mongoose.connect('mongodb+srv://ramanthdb:mz4258%2EY%2AX%5D%23S%3FH@cluster0-mhiwr.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://localhost:27017/basic-chat', { useNewUrlParser : true, useUnifiedTopology : true }, () => {
         console.log('connected to db')
     })
 }
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express()
 const Message = require('./app/models/Message')
-const port = 3900
+const port = process.env.PORT || 3900
 const decrypt = require('./app/middlewares/decryption')
 const cors = require('cors')
+const path = require('path')
 const setUpDB = require('./config/database')
 const socket = require('socket.io')
 
 setUpDB()
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static(path.join(__dirname,"client/build"))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
+}) 
+
 const server = app.listen(port, () => {
     console.log('connected to port', port)
 })
