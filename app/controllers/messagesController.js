12f5ffe96a583b07ca8626c22fa1d10908b7d659
Ramnath K <ramnath2609@gmail.com>
2020-03-02 12:41:16 +0530

    final

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -2,19 +2,23 @@ const Message = require('../models/Message')
 const encryption = require('../middlewares/encryption')
 
 module.exports.list = async () => {
-    const messages = await Message.find()
-    messages.forEach(msg => {
-        msg.body = encryption(msg.body)
-    })
-    return Promise.resolve(messages)
-    
+    return Message.find()
+                .then(messages => {
+                    messages.forEach(msg => {
+                        msg.body = encryption(msg.body)
+                    })
+                    return Promise.resolve(messages)
+                })  
 }
 
 
 module.exports.create = async (data) => {
     const message = new Message(data)
-    const msg = await message.save()
-    msg.body = encryption(msg.body)
-    return Promise.resolve(msg)
-
+    return message.save()
+                .then(msg => {
+                    msg.body = encryption(msg.body)
+                    return Promise.resolve(msg)
+                })       
 }
+    
+    
