b4a50ad49a6cfab15213d8e9285a9efb164d9139
Ramnath K <ramnath2609@gmail.com>
2020-03-02 20:12:29 +0530

    latest

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const socket = require('socket.io')
 
 setUpDB()
 
-// app.use(express.static(path.join(__dirname,"client/build"))) 
+app.use(express.static(path.join(__dirname,"client/build"))) 
 app.use(express.json())
 app.use(cors())
 
